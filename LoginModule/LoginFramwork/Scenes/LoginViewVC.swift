91d50ccae73f2e96b3804693a151f0441df8db5f
HoangNguyen <hoanguit@gmail.com>
2021-06-02 00:23:23 +0700

    no message

--- LoginModule/LoginFramwork/Scenes/LoginViewVC.swift
+++ LoginModule/LoginFramwork/Scenes/LoginViewVC.swift
@@ -15,12 +15,11 @@ public class LoginViewVC: UIViewController {
         super.viewDidLoad()
     }
     public func doLoginAction(userName: String,password: String,completion: @escaping (String) -> Void) {
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+    
             self.viewModel.doLogin(param: LoginParameters(userName: "", password: ""), completion: { result in
                 completion(result)
             })
-        }
+       
     }
     @IBAction func doLoginAction(_ sender: Any) {
         
